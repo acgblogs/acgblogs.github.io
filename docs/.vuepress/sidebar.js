2a04d73b16c80d33bf5aa93f4787062e46160d6d
codegeass <188120554@qq.com>
2021-03-04 09:30:19 +0800

    mark tensorflow.js

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -59,50 +59,15 @@ function prefixPath(basePath, dirPath) {
 }
 
 
-// const { getChildren } = require("vuepress-sidebar-atuo")
-
 module.exports = {
     '/blogs/': [{
         title: '基础知识',
         collapsable: true,
         children: getChildren('./docs/blogs')
+    }],
+    '/advance/': [{
+        title: '前沿技术',
+        collapsable: true,
+        children: getChildren('./docs/advance')
     }]
 }
-
-// module.exports = {
-//     '/views/': [{
-//             title: '基础知识',
-//             collapsable: true,
-//             children: getChildren('./docs/views/basis/')
-//         },
-//         {
-//             title: '高级进阶',
-//             collapsable: true,
-//             children: getChildren('./docs/views/advanced/')
-//         },
-//         {
-//             title: 'Bug',
-//             collapsable: true,
-//             children: getChildren('./docs/views/Bug/')
-//         },
-//         {
-//             title: '面试宝典',
-//             collapsable: true,
-//             children: getChildren('./docs/views/interview/')
-//         },
-//     ],
-//     '/coms/': [{
-//         title: '基础组件',
-//         collapsable: true,
-//         children: getChildren('./docs/coms/basicsCom/')
-//     }, {
-//         title: '业务组件',
-//         collapsable: true,
-//         children: getChildren('./docs/coms/professionCom/')
-//     }, ],
-//     '/jottings/': [{
-//         title: '随笔',
-//         collapsable: true,
-//         children: getChildren('./docs/jottings/')
-//     }]
-// }
---
+++ docs/advance/tensorflow.md
@@ -0,0 +1,10 @@
+---
+title: 人工智能 - TensorFlow.js
+date: '2021-08-08 08:00:00'
+sidebar: true
+tags:
+ - 前沿计算
+ - 人工智能
+---
+
+前端中的人工智能 - TensorFlow.js
